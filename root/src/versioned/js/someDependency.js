ca18a04f1f1447b2a59e84b3fba46dbdcff6c8c1
Rich Harris <richard.a.harris@gmail.com>
2013-08-13 11:38:48 +0100

    tweak

--- root/src/versioned/js/someDependency.js
+++ root/src/versioned/js/someDependency.js
@@ -6,6 +6,8 @@ function () {
 
 	'use strict';
 
-	return 'here we go!';
+	// This file just exists to show AMD modules in action. You should get rid of it!
+
+	return '<h1>success!</h1><p>The <strong>{%= name %}</strong> project has been created</p>';
 
 });
